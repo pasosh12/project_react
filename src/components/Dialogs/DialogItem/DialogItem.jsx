30903fd365d59ac722c922e558b908ef6d7befbd
pasosh12 <daniil.shishaev@gmail.com>
2021-10-17 23:25:24 +0300

    class component

---
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import classes from '../Dialog.module.css';
+import {NavLink} from "react-router-dom";
+
+const Dialog_item= (props) => {
+    let path="/dialogs/" + props.id;
+    return(
+        <div className={classes.dialog + ' ' + classes.active}>
+            <NavLink to={path}> {props.name} </NavLink>
+        </div>
+    );
+}
+const Message=(props)=>{
+    return(
+        <div className={classes.message}>{props.message}</div>
+    );
+}
+
+
+const Dialogs = (props)=> {
+
+
+    let dialogs = [
+            {id:1, name:"Дима"},
+            {id:2, name:"Саша"},
+            {id:3, name:"Петя"},
+            {id:4, name:"Игорь"},
+            {id:5, name:"Вячеслав"}
+            ]
+
+    let messages = [
+        {id:1, message:'Hi'},
+        {id:2, message:'Hello'},
+        {id:3, message:'React Cool!'}
+        ]
+
+    let dialogsElement = dialogs.map( d => <Dialog_item id={d.id} name={d.name} /> );
+
+
+    let messageElement = messages.map( m => <Message id={m.id} message={m.message} /> )
+
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialog_item}>
+                { dialogsElement }
+            </div>
+
+            <div className={classes.messages}>
+                { messageElement }
+            </div>
+
+        </div>
+    );
+}
+export default Dialogs;
---
+++ src/redux/store.js
@@ -0,0 +1,54 @@
+import profileReducer from "./profileReducer";
+import dialogsReducer from "./dialogsReducer";
+
+let store = {
+    _state : {
+       dialogsPage: {
+            dialogs: [
+                {id: 1, name: "Дима"},
+                {id: 2, name: "Саша"},
+                {id: 3, name: "Петя"},
+                {id: 4, name: "Игорь"},
+                {id: 5, name: "Вячеслав"}
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'Hello'},
+                {id: 3, message: 'React Cool!'}
+            ],
+            newMessageText:'Some Message'
+        },
+        profilePage: {
+            postsData: [
+                {id: 1, message: 'Hello!', likescount: 10},
+                {id: 2, message: "It's my first Post", likescount: 15}
+            ],
+            newPostText: 'Some post text'
+        }
+    },
+
+
+    _callSubscriber(){
+        console.log("State changed");
+        },
+    getState(){
+        return this._state;
+        },
+    subscribe(observer){
+        this._callSubscriber=observer;    // паттерн наблюдатель
+    },
+
+    dispatch(action){
+
+        this._state.profilePage = profileReducer(this.getState().profilePage, action);
+debugger;
+        this._state.dialogsPage = dialogsReducer(this.getState().dialogsPage, action);
+        this._callSubscriber(this._state);
+    },
+
+}
+
+
+let temp;
+window.temp=store;
+export default store;
